072809c6a4baebc2d2f4dd03da7ae023ad2d0cd3
Avah Whitehead <avah.whitehead@crm-uk.com>
2023-12-04 12:36:02 +0000

    added code to choose between running puzzle 1 and 2

--- puzzle1.py
+++ puzzle1.py
@@ -1,21 +1,26 @@
+IS_PUZZLE_2 = True
+
 digits = [str(i) for i in range(9 + 1)]
 
-digitsmap = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-}
+digitsmap = {}
 
-digits = [
-    *digits,
-    *[d for d in digitsmap.keys()]
-]
+if IS_PUZZLE_2:
+    digitsmap = {
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+    }
+
+    digits = [
+        *digits,
+        *[d for d in digitsmap.keys()]
+    ]
 
 def get_first_digit(s: str):
     for i in range(len(s)):
