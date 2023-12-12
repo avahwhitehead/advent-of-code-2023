5755871a0f034a7a347db4f3463af188ab2879f0
Avah Whitehead <avah.whitehead@crm-uk.com>
2023-12-12 14:09:37 +0000

    Added solution to day 7 puzzle 2

--- day7.py
+++ day7.py
@@ -22,7 +22,7 @@ def count_cards(cards: list[str]) -> { str: int }:
     return res
 
 
-def get_hand_strength(cards: list[str]) -> int:
+def get_hand_strength(cards: list[str], jokersWild: bool) -> int:
     freq = count_cards(cards)
 
     if len(freq) == 1:
@@ -34,39 +34,62 @@ def get_hand_strength(cards: list[str]) -> int:
         if freq[max(freq, key=freq.get)] == 4:
             # Four of a kind
             # (4 the same, 1 different)
+            if jokersWild and 'J' in freq:
+                # Can be converted to five of a kind
+                return 6
             return 5
         if freq[max(freq, key=freq.get)] == 3:
             # Full house
             # (3 the same, 2 sharing a label)
+            if jokersWild and 'J' in freq:
+                # Can be converted to five of a kind
+                return 6
             return 4
 
     if len(freq) == 3:
         if freq[max(freq, key=freq.get)] == 3:
             # Three of a kind
             # (3 the same, 2 different)
+            if jokersWild and 'J' in freq:
+                # Can be converted to four of a kind
+                return 5
             return 3
         
         if len([i for i in freq.keys() if freq[i] == 2]) == 2:
             # Two pair
             # (2 pairs of labels and one distinct label)
+            if jokersWild and 'J' in freq:
+                if freq['J'] == 2:
+                    # Can be converted to four of a kind
+                    return 5
+                # Can be converted to full house
+                return 4
             return 2
         
     if len(freq) == 4:
         # One pair
         # One pair of cards, and 3 distinct cards
+        if jokersWild and 'J' in freq:
+            # Can be converted to three of a kind
+            return 3
         return 1
 
     if len(freq) == 5:
         # High card
         # All distinct cards
+        if jokersWild and 'J' in freq:
+            # Can be converted to one pair
+            return 1
         return 0
     
     raise ValueError("Unknown hand combination: " + str(cards))
 
 
-def compare_hands(cards1: list[str], cards2: list[str]) -> int:
-    strength1 = get_hand_strength(cards1)
-    strength2 = get_hand_strength(cards2)
+def compare_hands(cards1: list[str], cards2: list[str], jokersWild: bool) -> int:
+    strength1 = get_hand_strength(cards1, jokersWild)
+    strength2 = get_hand_strength(cards2, jokersWild)
+    #print("\t\t", cards1, strength1)
+    #print("\t\t", cards2, strength2)
     
     #Cards 1 is stronger than cards 2
     if strength1 > strength2:
@@ -77,7 +100,10 @@ def compare_hands(cards1: list[str], cards2: list[str]) -> int:
         return 1
     
     #Order of cards from strongest to weakest
-    order = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+    if jokersWild:
+        order = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+    else:
+        order = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
 
     for c1, c2 in zip(cards1, cards2):
         #Index of each card
@@ -90,31 +116,39 @@ def compare_hands(cards1: list[str], cards2: list[str]) -> int:
         if c1 > c2:
             return 1
     
-    # raise ValueError("Hands have the same value:", cards1, cards2)
-    return 0
+    raise ValueError("Hands have the same value:", cards1, cards2)
+    # return 0
 
-def sort_hands(hands: list[list[str]]) -> list[list[str]]:
+def sort_hands(hands: list[list[str]], jokersWild: bool) -> list[list[str]]:
     res = []
     #just do a simple insertion sort here
     for hand in hands:
+        #print("inserting", hand)
         i = 0
         while i < len(res):
-            if compare_hands(res[i], hand) <= 0:
+            #print("\ttesting", res[i], "and", hand, compare_hands(res[i], hand, jokersWild))
+            if compare_hands(res[i], hand, jokersWild) <= 0:
                 break
             i += 1
         
         res.insert(i, hand)
+        #print(res)
+        #print()
 
     return res
 
 
 if __name__ == "__main__":
+    IS_PUZZLE_2 = True
+
     lines = get_input()
 
     data = parse_input(lines)
 
-    sorted_hands = sort_hands([c for c, b in data])
+    sorted_hands = sort_hands([c for c, b in data], IS_PUZZLE_2)
 
+    #print()
+    #print()
     winnings = 0
     for i, hand in enumerate(sorted_hands):
         bid = None
@@ -123,7 +157,7 @@ if __name__ == "__main__":
                 bid = b
                 break
         
-        print(i + 1, hand, bid, (i + 1) * bid)
+        #print(i + 1, hand, bid, (i + 1) * bid)
 
         winnings += (i + 1) * bid
 
